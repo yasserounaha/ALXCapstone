0dc26a4ea23e64ac62553dcc555650c3a952b155
yasserounaha <yasser12001@gmail.com>
2024-08-12 15:07:48 +0100

    Add and test function-based and class-based views for books and library details

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -11,5 +11,10 @@ def home(request):
     return render(request, 'home.html')
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = Book.objects.filter(library=self.object)
+        return context
