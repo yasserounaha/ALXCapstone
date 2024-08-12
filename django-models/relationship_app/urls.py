444d8133415050125e806be7762b7743109099c7
yasserounaha <yasser12001@gmail.com>
2024-08-12 15:11:01 +0100

    Add and test function-based and class-based views for books and library details

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path , include
-from .views import home ,list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView , home
 
 urlpatterns = [
     path('', home, name='home'),  # Root URL
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+     path('books/', list_books, name='list_books'),  # URL pattern for the function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL pattern for the class-based view
 ]
