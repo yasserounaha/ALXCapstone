cd24c113720332e9532232f2930d852369023d43
yasserounaha <yasser12001@gmail.com>
2024-08-14 16:47:56 +0100

    add new

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from .models import Book, Library
 from django.views.generic import DetailView
 
