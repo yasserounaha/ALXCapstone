11a4c82af706fc0b2699dd7d1a908d0eec54a277
yasserounaha <yasser12001@gmail.com>
2024-08-14 16:29:35 +0100

    add new

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path , include
 from .views import UserLoginView, UserLogoutView, UserRegisterView,list_books, LibraryDetailView , home
 
 urlpatterns = [
-     path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('register/', UserRegisterView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('', home, name='home'),  # Root URL
      path('books/', list_books, name='list_books'),  # URL pattern for the function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL pattern for the class-based view
