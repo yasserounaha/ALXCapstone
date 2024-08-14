51a429a692ccec3bc361748b35b7da06c45cc35b
yasserounaha <yasser12001@gmail.com>
2024-08-14 16:27:03 +0100

    add new

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path , include
-from .views import list_books, LibraryDetailView , home
+from .views import UserLoginView, UserLogoutView, UserRegisterView,list_books, LibraryDetailView , home
 
 urlpatterns = [
+     path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', UserRegisterView.as_view(), name='register'),
     path('', home, name='home'),  # Root URL
      path('books/', list_books, name='list_books'),  # URL pattern for the function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL pattern for the class-based view
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,9 +1,28 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
 from django.shortcuts import render
 from .models import Book
 from django.views.generic import DetailView
 from .models import Library
 from django.views.generic.detail import DetailView
 
+class UserLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+class UserLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+class UserRegisterView(FormView):
+    template_name = 'relationship_app/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
