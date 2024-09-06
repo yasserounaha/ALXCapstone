a71ed310ebbe155f8a4469fe9a39a189684719c5
yasserounaha <yasser12001@gmail.com>
2024-09-06 03:14:00 +0100

    new

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -24,7 +24,7 @@ class ListView(ListAPIView):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [filters.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['title', 'author', 'publication_year']
     search_fields = ['title', 'author']
     ordering_fields = ['title', 'publication_year']
