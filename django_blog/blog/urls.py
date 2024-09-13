aad75e9d3c90d984d0d8cef6c9cbd0a03583533c
yasserounaha <yasser12001@gmail.com>
2024-09-13 15:52:48 +0100

    New

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts_by_tag'),
     path('search/', search, name='search'),
-    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
 ]
 
