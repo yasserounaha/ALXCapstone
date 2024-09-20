ac495d89455d94699f7cf8430c25a58a731aff2e
yasserounaha <yasser12001@gmail.com>
2024-09-20 15:48:19 +0100

    New

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
 
-# Create your views here.
+class ViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing, creating, updating, and deleting posts.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Assign the post to the current user
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing, creating, updating, and deleting comments.
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Assign the comment to the current user
+        serializer.save(author=self.request.user)
+
