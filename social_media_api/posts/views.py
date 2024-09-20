6168fb92b6dd6129c9c6b59df585353f178183b4
yasserounaha <yasser12001@gmail.com>
2024-09-20 15:57:13 +0100

    New

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -4,6 +4,8 @@ from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import GenericAPIView
+
 class ViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing, creating, updating, and deleting posts.
