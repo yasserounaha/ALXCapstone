c84eab142ca883b9b9b241209c5e3743f78b3847
yasserounaha <yasser12001@gmail.com>
2024-09-21 04:55:26 +0100

    New

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -3,9 +3,11 @@ from .views import RegisterView, CustomLoginView
 from rest_framework.routers import DefaultRouter
 from .views import follow_user, unfollow_user
 from . import views
+from .views import FollowUserView, UnfollowUserView
+
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
-    path('follow/<int:user_id>/', follow_user, name='follow-user'),
-    path('unfollow/<int:user_id>/', unfollow_user, name='unfollow-user'),
+     path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow_user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow_user'),
 ]
